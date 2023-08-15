15ce79aa762ad853f4b00f4209b11537f0958fd3
Di Depan Layar <albinmaulana@yahoo.com>
2023-08-15 10:41:37 +0700

    Feature: Install Redux Framework

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
+    include 'opt/redux-core/framework.php';
+    include 'opt/sample/r-config.php';
+
     add_theme_support('menus');
     add_theme_support('widgets');
     add_theme_support('title-tag');
